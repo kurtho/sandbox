dd359fe61ab6b20adb627c244bd80c7532e32b51
kurt ho <ahopdanzer@gmail.com>
2016-05-31 14:33:21 +0800

    ACApp2

---
+++ ACApp2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ACApp2
+//
+//  Created by KurtHo on 2016/5/28.
+//  Copyright © 2016年 Kurt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let imageName = ["0","1", "2","3","4","5","6","7","8","9"]
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
